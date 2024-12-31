7c0cb9eec48fff257f0cefdbb3fa607540f62156
sultanmahmud07 <asadkhan01862@gmail.com>
2024-12-31 23:45:04 +0600

    New domain added

--- Constants.js
+++ Constants.js
@@ -1,5 +1,6 @@
 
-const BASEURL ="http://147.79.117.103:8002/v1"              //live server connect
+// const BASEURL ="http://147.79.117.103:8002/v1"              //live server connect
+const BASEURL ="https://api.gdkyd.com/v1"              //live server connect
 
 
 export default BASEURL;
